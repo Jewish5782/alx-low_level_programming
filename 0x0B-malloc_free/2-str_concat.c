3aba95e702b4f0e2f857c8f5784b5cb3448d8f9a
Felix Pronk <felixpronk@Felixs-MacBook-Pro.local>
2023-10-05 17:38:31 +0000

    Task 2 again update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,47 +10,47 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    int i;
-    char *result;
-    int size1 = 0;
-    int size2 = 0;
+	int i;
+	char *result;
+	int size1 = 0;
+	int size2 = 0;
 
-    if (s1 != NULL)
-    {
-        while (s1[size1])
-        {
-            size1++;
-        }
-    }
+	if (s1 != NULL)
+	{
+		while (s1[size1])
+		{
+			size1++;
+		}
+	}
 
-    if (s2 != NULL)
-    {
-        while (s2[size2])
-        {
-            size2++;
-        }
-    }
+	if (s2 != NULL)
+	{
+		while (s2[size2])
+		{
+			size2++;
+		}
+	}
 
-    result = malloc(size1 + size2 + 1);
+	result = malloc(size1 + size2 + 1);
 
-    if (!result)
-    {
-        return (NULL);
-    }
+	if (!result)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < size1 + size2; i++)
-    {
-        if (s1 != NULL && i < size1)
-        {
-            result[i] = s1[i];
-        }
-        else if (s2 != NULL)
-        {
-            result[i] = s2[i - size1];
-        }
-    }
+	for (i = 0; i < size1 + size2; i++)
+	{
+		if (s1 != NULL && i < size1)
+		{
+			result[i] = s1[i];
+		}
+		else if (s2 != NULL)
+		{
+			result[i] = s2[i - size1];
+		}
+	}
 
-    result[size1 + size2] = '\0';
+	result[size1 + size2] = '\0';
 
-    return (result);
+	return (result);
 }
