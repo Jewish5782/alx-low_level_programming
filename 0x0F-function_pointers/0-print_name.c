529587417e186aaf07dd7154475be523e0226be3
Jewish5782 <pronkfelix@gmail.com>
2023-11-22 15:57:43 +0000

    Update fix function pointers for 0,1,and 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * print_name - Prints a name using a function pointer.
- * @name: The string to print.
- * @printFunction: Pointer to the function for printing.
- * Return: Nothing.
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
  **/
-void print_name(char *name, void (*printFunction)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && printFunction != NULL)
-    {
-        printFunction(name);
-    }
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,21 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * array_iterator - Applies a given function to each element of an array.
- * @array: The array to iterate over.
- * @size: The number of elements in the array.
- * @action: Pointer to the function to apply.
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    if (array != NULL && action != NULL)
-    {
-        for (size_t i = 0; i < size; ++i)
-        {
-            action(array[i]);
-        }
-    }
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
