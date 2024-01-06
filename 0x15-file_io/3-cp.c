298b079e2f509d2dcdd3f04c61ef6c5d514b9e89
Jewish5782 <pronkfelix@gmail.com>
2024-01-06 15:47:17 +0000

    Task 3 update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,76 +1,103 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
-#define BUFFER_SIZE 1024
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * print_usage_and_exit - Print usage message and exit with code 97.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
-void print_usage_and_exit(void)
+char *create_buffer(char *file)
 {
-	dprintf(2, "Usage: cp file_from file_to\n");
-	exit(97);
-}
+	char *buffer;
 
-/**
- * print_error_and_exit - Print error message and exit with the given code.
- * @message: The error message to print.
- * @exit_code: The exit code to use.
- */
-void print_error_and_exit(const char *message, int exit_code)
-{
-	dprintf(2, "%s\n", message);
-	exit(exit_code);
-}
+	buffer = malloc(sizeof(char) * 1024);
 
-/**
- * close_files_and_exit - Close file descriptors and exit with the given code.
- * @fd_from: The source file descriptor.
- * @fd_to: The destination file descriptor.
- * @exit_code: The exit code to use.
- */
-void close_files_and_exit(int fd_from, int fd_to, int exit_code)
-{
-	close(fd_from);
-	close(fd_to);
-	exit(exit_code);
-}
-
-int main(int argc, char *argv[])
-{
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
-	if (argc != 3)
-		print_usage_and_exit();
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		print_error_and_exit("Error: Can't read from file", 98);
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		print_error_and_exit("Error: Can't write to file", 99);
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	if (buffer == NULL)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-			close_files_and_exit(fd_from, fd_to, 99);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
 	}
 
-	if (bytes_read == -1)
-		close_files_and_exit(fd_from, fd_to, 98);
+	return (buffer);
+}
 
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-		print_error_and_exit("Error: Can't close fd", 100);
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
 
-	chmod(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
